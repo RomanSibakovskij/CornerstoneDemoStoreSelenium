35fc6dcdaf1f0fa88d5147280a5219d4852dab86
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:33:20 +0300

    Add empty LoginRegisterDashboardPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/LoginRegisterDashboardPage.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class LoginRegisterDashboardPage extends BasePage {
+
+
+    public LoginRegisterDashboardPage(WebDriver driver) {super(driver);}
+
+    
+}
