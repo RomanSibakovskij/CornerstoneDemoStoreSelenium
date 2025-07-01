3854a6c97f66d0a93addb11309d08f396c2d19ee
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:18:27 +0300

    Add empty HomePage sheet

---
+++ src/main/java/com/mybigcommerce/demo/HomePage.java
@@ -0,0 +1,16 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class HomePage extends BasePage {
+
+
+
+    public HomePage(WebDriver driver) {super(driver);}
+
+    
+}
