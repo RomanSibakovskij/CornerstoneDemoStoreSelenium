1c6cf4181044256fe6da6fdbacaca0f5b6fafffa
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:37:49 +0300

    Add empty RegisterPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/RegisterPage.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPage extends BasePage{
+
+
+
+    public RegisterPage(WebDriver driver) {super(driver);}
+
+    
+}
