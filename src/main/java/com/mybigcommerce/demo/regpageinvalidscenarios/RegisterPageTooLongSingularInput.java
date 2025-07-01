8f8bf5e119e199bf54f143d55f25cb542aa1b933
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 06:01:11 +0300

    Add empty RegisterPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/regpageinvalidscenarios/RegisterPageTooLongSingularInput.java
@@ -0,0 +1,13 @@
+package com.mybigcommerce.demo.regpageinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPageTooLongSingularInput extends BasePage{
+
+
+    public RegisterPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+}
