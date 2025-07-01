8d3f82a6d99adb3a3da4de5a075e01e00ef46843
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:42:32 +0300

    Add empty TestDataGenerator utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/utilities/TestDataGenerator.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo.utilities;
+
+import org.openqa.selenium.*;
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.util.*;
+
+public class TestDataGenerator extends BasePage{
+
+    
+
+    public TestDataGenerator(WebDriver driver) {super(driver);}
+
+
+}
