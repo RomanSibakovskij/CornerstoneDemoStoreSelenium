d4d8b2aef655a74b391b45a61dacf4fab14f06f9
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 17:56:38 +0300

    Add empty RegisterPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/regpageinvalidscenarios/RegisterPageInvalidSingularInputFormat.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.regpageinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPageInvalidSingularInputFormat extends BasePage{
+
+
+
+    public RegisterPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+
+}
