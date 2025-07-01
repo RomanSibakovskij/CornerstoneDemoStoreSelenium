c7da18e85695bf8009dbbc3c369648b6dbf3f436
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:14:10 +0300

    Add empty GeneralPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/GeneralPage.java
@@ -0,0 +1,12 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class GeneralPage extends BasePage {
+
+
+    public GeneralPage(WebDriver driver) {super(driver);}
+
+    
+}
