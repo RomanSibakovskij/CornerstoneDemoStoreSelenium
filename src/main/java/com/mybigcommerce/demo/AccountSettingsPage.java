cdaacb79ef88bcc9ae00bae45fe9aab17603b351
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 18:34:17 +0300

    Add empty AccountSettingsPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/AccountSettingsPage.java
@@ -0,0 +1,16 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class AccountSettingsPage extends BasePage {
+
+    
+
+    public AccountSettingsPage(WebDriver driver) {super(driver);}
+
+
+}
