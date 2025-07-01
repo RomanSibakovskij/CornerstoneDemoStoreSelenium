6fc702d7ef73f9f86463a0a47077a6b5d8e9e1d6
RomanSibakovskij <benutzer373@gmail.com>
2025-07-01 04:54:30 +0300

    Add empty RegisterPageNoSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/regpageinvalidscenarios/RegisterPageNoSingularInput.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.regpageinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPageNoSingularInput extends BasePage{
+
+
+
+    public RegisterPageNoSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
