5c6d3577267a964565b1cba477358d5501ac4685
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 18:04:43 +0300

    Add empty AddAddressPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/addaddresspageinvalidscenarios/AddAddressPageInvalidSingularInputFormat.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.addaddresspageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class AddAddressPageInvalidSingularInputFormat extends BasePage{
+
+
+    public AddAddressPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+
+}
