eb6c31b9ec227a1669b9d54234839db163b759a4
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 04:44:16 +0300

    Add relevant web elements for AccountSettingsPageTooShortSingularInput utility sheet

--- src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingsPageTooShortSingularInput.java
+++ src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingsPageTooShortSingularInput.java
@@ -7,7 +7,23 @@ import org.openqa.selenium.support.FindBy;
 
 public class AccountSettingsPageTooShortSingularInput extends BasePage{
 
-
+    //input form
+    @FindBy(xpath = "//input[@id='account_firstname']")
+    private WebElement accountSettingsFirstNameInputField;
+    @FindBy(xpath = "//input[@id='account_lastname']")
+    private WebElement accountSettingsLastNameInputField;
+    @FindBy(xpath = "//input[@id='account_companyname']")
+    private WebElement accountSettingsCompanyInputField;
+    @FindBy(xpath = "//input[@id='account_phone']")
+    private WebElement accountSettingsPhoneInputField;
+    @FindBy(xpath = "//input[@id='FormField_1_input']")
+    private WebElement accountSettingsEmailInputField;
+    @FindBy(xpath = "//input[@id='FormField_2_input']")
+    private WebElement accountSettingsPasswordInputField;
+    @FindBy(xpath = "//input[@id='FormField_3_input']")
+    private WebElement accountSettingsConfirmPasswordInputField;
+    @FindBy(xpath = "//input[@id='FormField_24_input']")
+    private WebElement accountSettingsCurrentPasswordInputField;
 
     public AccountSettingsPageTooShortSingularInput(WebDriver driver) {super(driver);}
 
