7a1d859389432b9cfbac0f3208baef4d9361b127
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 17:19:12 +0300

    Add relevant web element for AddAddressPageTooShortSingularInput utility sheet

--- src/main/java/com/mybigcommerce/demo/addaddresspageinvalidscenarios/AddAddressPageTooShortSingularInput.java
+++ src/main/java/com/mybigcommerce/demo/addaddresspageinvalidscenarios/AddAddressPageTooShortSingularInput.java
@@ -7,8 +7,21 @@ import org.openqa.selenium.support.FindBy;
 
 public class AddAddressPageTooShortSingularInput extends BasePage{
 
+    //input form
+    @FindBy(xpath = "//input[@id='FormField_4_input']")
+    private WebElement addAddressPageFirstNameInputField;
+    @FindBy(xpath = "//input[@id='FormField_5_input']")
+    private WebElement addAddressPageLastNameInputField;
+    @FindBy(xpath = "//input[@id='FormField_8_input']")
+    private WebElement addAddressPageAddress1InputField;
+    @FindBy(xpath = "//input[@id='FormField_10_input']")
+    private WebElement addAddressPageCityInputField;
+    @FindBy(xpath = "//input[@id='FormField_13_input']")
+    private WebElement addAddressPagePostCodeInputField;
+    @FindBy(xpath = "//input[@id='FormField_7_input']")
+    private WebElement addAddressPagePhoneInputField;
 
     public AddAddressPageTooShortSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
