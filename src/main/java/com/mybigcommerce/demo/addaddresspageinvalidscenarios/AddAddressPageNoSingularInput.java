35dd4859cdc5103406747e2494d68af375b4a572
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 05:57:45 +0300

    Add empty AddAddressPageNoSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/addaddresspageinvalidscenarios/AddAddressPageNoSingularInput.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.addaddresspageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class AddAddressPageNoSingularInput extends BasePage{
+
+
+    public AddAddressPageNoSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
