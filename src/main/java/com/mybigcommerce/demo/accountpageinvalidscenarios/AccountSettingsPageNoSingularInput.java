7e08cbddf9fb55b78cb6fcd05ac59a935b9b2d6c
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 04:18:56 +0300

    Add relevant web elements for AccountSettingsPageNoSingularInput utility sheet

--- src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingsPageNoSingularInput.java
+++ src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingsPageNoSingularInput.java
@@ -7,8 +7,25 @@ import org.openqa.selenium.support.FindBy;
 
 public class AccountSettingsPageNoSingularInput extends BasePage{
 
+    //input form
+    @FindBy(xpath = "//input[@id='account_firstname']")
+    private WebElement accountSettingsFirstNameInputField;
+    @FindBy(xpath = "//input[@id='account_lastname']")
+    private WebElement accountSettingsLastNameInputField;
+    @FindBy(xpath = "//input[@id='account_companyname']")
+    private WebElement accountSettingsCompanyInputField;
+    @FindBy(xpath = "//input[@id='account_phone']")
+    private WebElement accountSettingsPhoneInputField;
+    @FindBy(xpath = "//input[@id='FormField_1_input']")
+    private WebElement accountSettingsEmailInputField;
+    @FindBy(xpath = "//input[@id='FormField_2_input']")
+    private WebElement accountSettingsPasswordInputField;
+    @FindBy(xpath = "//input[@id='FormField_3_input']")
+    private WebElement accountSettingsConfirmPasswordInputField;
+    @FindBy(xpath = "//input[@id='FormField_24_input']")
+    private WebElement accountSettingsCurrentPasswordInputField;
 
     public AccountSettingsPageNoSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
