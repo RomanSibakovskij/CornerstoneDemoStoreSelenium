348dde6d8b0798f97d997a68b941a74ef8a2b018
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 05:18:11 +0300

    Add empty AccountSettingPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingPageInvalidSingularInputFormat.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo.accountpageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountSettingPageInvalidSingularInputFormat extends BasePage{
+
+
+
+    public AccountSettingPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+
+}
