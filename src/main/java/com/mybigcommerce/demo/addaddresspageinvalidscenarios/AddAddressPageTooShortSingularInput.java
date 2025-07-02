dc259f75dc7bf70a468205696e152598fdcaebaf
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 17:18:18 +0300

    Add empty AddAddressPageTooShortSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/addaddresspageinvalidscenarios/AddAddressPageTooShortSingularInput.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.addaddresspageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class AddAddressPageTooShortSingularInput extends BasePage{
+
+
+    public AddAddressPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
