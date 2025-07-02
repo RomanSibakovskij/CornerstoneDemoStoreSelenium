56c6e1de86f644d7d7878f251a6e68a1d31f14c6
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 04:42:47 +0300

    Add empty AccountSettingsPageTooShortSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/accountpageinvalidscenarios/AccountSettingsPageTooShortSingularInput.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo.accountpageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountSettingsPageTooShortSingularInput extends BasePage{
+
+
+
+    public AccountSettingsPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+
+}
