6917e49b504ad710656befe7bc99011dd2d38932
RomanSibakovskij <benutzer373@gmail.com>
2025-07-02 05:42:07 +0300

    Add empty AddAddressPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/AddAddressPage.java
@@ -0,0 +1,17 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class AddAddressPage extends BasePage {
+
+
+    public AddAddressPage(WebDriver driver) {super(driver);}
+
+
+}
