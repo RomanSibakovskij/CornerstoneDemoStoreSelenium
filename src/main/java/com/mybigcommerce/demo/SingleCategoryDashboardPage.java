6e9ca7ca0cad4cfaae7c10be769d0c4df6955032
RomanSibakovskij <benutzer373@gmail.com>
2025-07-03 16:50:37 +0300

    Add empty SingleCategoryDashboardPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/SingleCategoryDashboardPage.java
@@ -0,0 +1,17 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SingleCategoryDashboardPage extends BasePage {
+
+
+    public SingleCategoryDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
