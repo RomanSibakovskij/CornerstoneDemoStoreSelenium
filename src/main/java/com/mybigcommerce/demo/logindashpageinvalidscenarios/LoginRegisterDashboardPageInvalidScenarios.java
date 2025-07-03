5832fcfef4e66d86f84f02b42f31793f42fa7fad
RomanSibakovskij <benutzer373@gmail.com>
2025-07-03 05:53:12 +0300

    Add empty LoginRegisterDashboardPageInvalidScenarios utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/logindashpageinvalidscenarios/LoginRegisterDashboardPageInvalidScenarios.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.logindashpageinvalidscenarios;
+
+import com.mybigcommerce.demo.RegisterPage;
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginRegisterDashboardPageInvalidScenarios extends BasePage {
+
+
+    public LoginRegisterDashboardPageInvalidScenarios(WebDriver driver) {super(driver);}
+
+
+}
