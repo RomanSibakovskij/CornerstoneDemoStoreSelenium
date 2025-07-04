57bb1557297901f376623388e9f42cd7891f555b
RomanSibakovskij <benutzer373@gmail.com>
2025-07-04 04:01:30 +0300

    Add empty AddToCompareListPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/AddToCompareListPage.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class AddToCompareListPage extends BasePage {
+
+
+    public AddToCompareListPage(WebDriver driver) {super(driver);}
+
+    
+}
