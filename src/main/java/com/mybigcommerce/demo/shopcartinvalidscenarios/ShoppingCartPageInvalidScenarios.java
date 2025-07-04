c69eeb055695182b6b8276148642d1450d2ba602
RomanSibakovskij <benutzer373@gmail.com>
2025-07-04 05:50:40 +0300

    Add relevant web elements for ShoppingCartPageInvalidScenarios utility sheet

--- src/main/java/com/mybigcommerce/demo/shopcartinvalidscenarios/ShoppingCartPageInvalidScenarios.java
+++ src/main/java/com/mybigcommerce/demo/shopcartinvalidscenarios/ShoppingCartPageInvalidScenarios.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.support.FindBy;
 
 public class ShoppingCartPageInvalidScenarios extends BasePage{
 
+    //shipping section
+    @FindBy(xpath = "//input[@id='shipping-city']")
+    private WebElement shoppingCartPageShippingCityInputField;
+    @FindBy(xpath = "//input[@id='shipping-zip']")
+    private WebElement shoppingCartPageShippingPostCodeInputField;
 
     public ShoppingCartPageInvalidScenarios(WebDriver driver) {super(driver);}
 
