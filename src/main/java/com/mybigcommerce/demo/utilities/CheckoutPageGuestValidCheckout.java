b9a4b647276b626d874a45b5be9e248ac4757a89
RomanSibakovskij <benutzer373@gmail.com>
2025-07-04 06:40:27 +0300

    Add empty CheckoutPageGuestValidCheckout utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/utilities/CheckoutPageGuestValidCheckout.java
@@ -0,0 +1,12 @@
+package com.mybigcommerce.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPageGuestValidCheckout extends BasePage{
+
+
+    public CheckoutPageGuestValidCheckout(WebDriver driver) {super(driver);}
+
+
+}
