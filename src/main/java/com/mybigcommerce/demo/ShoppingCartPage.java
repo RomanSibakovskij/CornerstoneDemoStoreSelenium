324ee8bbc5b9402747a5ec05f920233378ae4530
RomanSibakovskij <benutzer373@gmail.com>
2025-07-04 04:31:11 +0300

    Add empty ShoppingCartPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/ShoppingCartPage.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class ShoppingCartPage extends BasePage{
+
+
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+
+}
