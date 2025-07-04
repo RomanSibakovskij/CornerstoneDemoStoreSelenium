1cc555f95086c0ec92400eebdcf6b13cfdcf7467
RomanSibakovskij <benutzer373@gmail.com>
2025-07-04 06:27:50 +0300

    Add empty CheckoutPage sheet

---
+++ src/main/java/com/mybigcommerce/demo/CheckoutPage.java
@@ -0,0 +1,15 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class CheckoutPage extends BasePage{
+
+
+    public CheckoutPage(WebDriver driver) {super(driver);}
+
+
+}
