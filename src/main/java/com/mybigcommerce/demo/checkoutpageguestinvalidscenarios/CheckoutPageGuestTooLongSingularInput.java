48d3bb7ab6accd3539fe0c49eba20d5ba3adee78
RomanSibakovskij <benutzer373@gmail.com>
2025-07-05 16:36:27 +0300

    Add empty CheckoutPageGuestTooLongSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/checkoutpageguestinvalidscenarios/CheckoutPageGuestTooLongSingularInput.java
@@ -0,0 +1,13 @@
+package com.mybigcommerce.demo.checkoutpageguestinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPageGuestTooLongSingularInput extends BasePage{
+
+
+    public CheckoutPageGuestTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+}
