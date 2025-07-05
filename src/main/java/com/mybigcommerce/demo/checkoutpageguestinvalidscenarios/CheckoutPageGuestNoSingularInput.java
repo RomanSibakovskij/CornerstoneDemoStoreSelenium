beb2999479ac2ec8235612911bdc2d5a8ea788ac
RomanSibakovskij <benutzer373@gmail.com>
2025-07-05 04:13:33 +0300

    Add empty CheckoutPageGuestNoSingularInput utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/checkoutpageguestinvalidscenarios/CheckoutPageGuestNoSingularInput.java
@@ -0,0 +1,14 @@
+package com.mybigcommerce.demo.checkoutpageguestinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPageGuestNoSingularInput extends BasePage {
+
+
+
+    public CheckoutPageGuestNoSingularInput(WebDriver driver) {super(driver);}
+
+
+}
