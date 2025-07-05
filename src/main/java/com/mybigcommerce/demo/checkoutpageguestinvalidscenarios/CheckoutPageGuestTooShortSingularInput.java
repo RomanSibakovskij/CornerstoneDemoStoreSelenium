48224a020ea93bb036be9c26f1aae2076b4927f3
RomanSibakovskij <benutzer373@gmail.com>
2025-07-05 06:13:08 +0300

    Add Invalid Single Most Popular Product Addition To Checkout Test (as a guest) - Too Short First Name test method

--- src/main/java/com/mybigcommerce/demo/checkoutpageguestinvalidscenarios/CheckoutPageGuestTooShortSingularInput.java
+++ src/main/java/com/mybigcommerce/demo/checkoutpageguestinvalidscenarios/CheckoutPageGuestTooShortSingularInput.java
@@ -58,6 +58,7 @@ public class CheckoutPageGuestTooShortSingularInput extends BasePage{
 
     //invalid guest shipping address input data - too short singular input
     private String tooShortGuestEmail;
+    private String tooShortShippingGuestFirstName;
 
     public CheckoutPageGuestTooShortSingularInput(WebDriver driver) {super(driver);}
 
@@ -76,5 +77,41 @@ public class CheckoutPageGuestTooShortSingularInput extends BasePage{
 
     //invalid guest email address input method - too short guest email
     public void inputTooShortGuestEmailIntoEmailInputField(){checkoutPageGuestEmailInputField.sendKeys(tooShortGuestEmail);}
+
+    //shipping address input
+
+    //invalid guest shipping address input data getter - too short shipping first name (1 char)
+    public void invalidGuestShipAddressInputDataTooShortFirstNameGetter(){
+
+        tooShortShippingGuestFirstName = "S";
+        validShippingGuestLastName = TestDataGenerator.getRandomLastName();
+        validShippingGuestAddress = TestDataGenerator.generateRandomAddress(8);
+        validShippingGuestPhone = TestDataGenerator.generatePhoneNumber(9);
+
+        System.out.println("Invalid generated checkout guest shipping address input data (too short shipping first name): " + "\n");
+
+        logger.info("Too short shipping address first name: " + tooShortShippingGuestFirstName);
+        logger.info("Valid guest shipping address last name (too short shipping first name): " + validShippingGuestLastName);
+        logger.info("Valid guest shipping address (too short shipping first name): " + validShippingGuestAddress);
+        logger.info("Valid guest shipping address phone number (too short shipping first name): " + validShippingGuestPhone);
+
+        System.out.println("\n");
+
+    }
+
+    //invalid guest shipping data input methods - too short singular input
+    public void inputTooShortShippingGuestFirstNameIntoFirstNameInputField(){checkoutPageShippingFirstNameInputField.sendKeys(tooShortShippingGuestFirstName);}
+
+    //valid guest email address input method (for remaining inputs)
+    public void inputValidGuestEmailIntoEmailInputField(){checkoutPageGuestEmailInputField.sendKeys(validGuestEmail);}
+
+    //valid guest shipping data input methods (for remaining inputs)
+    public void inputValidShippingGuestFirstNameIntoFirstNameInputField(){checkoutPageShippingFirstNameInputField.sendKeys(validShippingGuestFirstName);}
+    public void inputValidShippingGuestLastNameIntoLastNameInputField(){checkoutPageShippingLastNameInputField.sendKeys(validShippingGuestLastName);}
+    public void inputValidShippingGuestAddressIntoAddressInputField(){checkoutPageShippingAddressInputField.sendKeys(validShippingGuestAddress);}
+    public void inputValidShippingGuestPhoneIntoPhoneInputField(){checkoutPageShippingPhoneInputField.sendKeys(validShippingGuestPhone);}
+
+
+
 
 }
--- src/test/java/com/mybigcommerce/demo/checkoutpageinvalidscenarios/CheckoutPageGuestTooShortSingularInputTest.java
+++ src/test/java/com/mybigcommerce/demo/checkoutpageinvalidscenarios/CheckoutPageGuestTooShortSingularInputTest.java
@@ -3,7 +3,7 @@ package com.mybigcommerce.demo.checkoutpageinvalidscenarios;
 import com.mybigcommerce.demo.utilities.TestMethods;
 import org.junit.jupiter.api.*;
 
-public class CheckoutPageGuestTooShortSingularInputTest extends TestMethods{
+public class CheckoutPageGuestTooShortSingularInputTest extends TestMethods {
 
     //too short singular input
 
@@ -20,6 +20,23 @@ public class CheckoutPageGuestTooShortSingularInputTest extends TestMethods{
         addProductToCheckoutTest();
         //invalid single most popular product ("Tiered Wire Basket") check out confirmation test method (as a guest) - too short guest email (1 char -> name, domain)
         invalidGuestProductOrderCheckoutTooShortGuestEmailTest();
+    }
+
+    //Test 029v -> invalid single most popular product ("Tiered Wire Basket") check out confirmation test method (as a guest) - too short guest shipping first name (1 char) (the error wasn't triggered, test has failed)
+    @Test
+    @DisplayName("Invalid Single Most Popular Product Addition To Checkout Test (as a guest) - Too Short Shipping First Name")
+    @Tag("Invalid_Single_Most_Popular_Product_Checkout_Confirmation")
+    @Tag("Too_Short_Singular_Input")
+    @Tag("Test_As_A_Guest")
+    void invalidSingleMostPopularProductCheckoutConfirmationTooShortGuestShipFirstNameTest() {
+        //single most popular product ("Tiered Wire Basket") addition to cart test method (as a guest)
+        addSingleMostPopularProductToCartGuestTest();
+        //single most popular product ("Tiered Wire Basket") addition to check out test method (as a guest)
+        addProductToCheckoutTest();
+        //invalid single most popular product ("Tiered Wire Basket") check out confirmation test method (as a guest) - too short guest shipping first name (1 char)
+        invalidGuestProductOrderCheckoutTooShortGuestShipFirstNameTest();
+
+
     }
 
 
