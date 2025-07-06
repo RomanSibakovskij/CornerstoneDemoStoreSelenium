9f4955fff9c16008d09866f5b04f0adb083e7c08
RomanSibakovskij <benutzer373@gmail.com>
2025-07-06 04:32:15 +0300

    Add empty CheckoutPageGuestInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/mybigcommerce/demo/checkoutpageguestinvalidscenarios/CheckoutPageGuestInvalidSingularInputFormat.java
@@ -0,0 +1,13 @@
+package com.mybigcommerce.demo.checkoutpageguestinvalidscenarios;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPageGuestInvalidSingularInputFormat extends BasePage{
+
+
+    public CheckoutPageGuestInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+    
+}
