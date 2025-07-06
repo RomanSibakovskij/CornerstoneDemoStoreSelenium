40bc4d08c6e49cce4e98a7f83a2de7e18d79d4b9
RomanSibakovskij <benutzer373@gmail.com>
2025-07-06 05:55:49 +0300

    Add empty OrderInvoicePage sheet

---
+++ src/main/java/com/mybigcommerce/demo/OrderInvoicePage.java
@@ -0,0 +1,13 @@
+package com.mybigcommerce.demo;
+
+import com.mybigcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class OrderInvoicePage extends BasePage{
+
+
+    public OrderInvoicePage(WebDriver driver) {super(driver);}
+
+
+}
