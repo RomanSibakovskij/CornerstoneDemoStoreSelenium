f891e2b3dc655d1ef9d62e6456081dd36510ef03
RomanSibakovskij <benutzer373@gmail.com>
2025-07-06 06:14:38 +0300

    Add Multiple New Products Order Submission Confirmation Test (as a registered user) method

--- src/test/java/com/mybigcommerce/demo/OrderInvoicePageTest.java
+++ src/test/java/com/mybigcommerce/demo/OrderInvoicePageTest.java
@@ -75,4 +75,26 @@ public class OrderInvoicePageTest extends TestMethods {
         orderInvoicePageOrderConfirmationTest();
     }
 
+    //Test 031a -> multiple new products ("Kinfolk Issue 16") order submission confirmation test method (as a registered user)
+    @Test
+    @DisplayName("Multiple New Products Order Submission Confirmation Test (as a registered user)")
+    @Tag("Multiple_New_Products_Checkout_Submission_Confirmation")
+    @Tag("Test_As_A_Reg_User")
+    void multipleNewProductsOrderConfirmationRegUserTest() {
+        //user navigation to register page test
+        navigateToRegisterPageTest();
+        //valid user account creation test
+        validUserAccountCreationTest();
+        //multiple new products ("Kinfolk Issue 16") addition to cart test method (as a registered user)
+        addMultipleNewProductsToCartRegUserTest();
+        //multiple new product ("Kinfolk Issue 16") addition to check out test method (as a registered user)
+        addProductToCheckoutTest();
+        //multiple new product ("Kinfolk Issue 16") check out confirmation test method (as a registered user)
+        validRegUserProductOrderCheckoutTest();
+        //navigate back to home page (with driver) (the header home link nor "Continue shopping" button seem to be working during automation run(manually, everything works)
+        navigateToHomePageTest();
+        //multiple new products ("Kinfolk Issue 16") order submission confirmation test method (as a registered user)
+        orderInvoicePageOrderConfirmationTest();
+    }
+
 }
